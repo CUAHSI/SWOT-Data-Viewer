16fd3042c4fcf30f098647e0593663ca2634af9a
Devin Cowan <dcowan@cuahsi.org>
2024-02-24 14:37:59 -0500

    add chart.js

---
+++ frontend/src/components/ChartVis.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-theme-provider theme="light" with-background>
+    <Line :data="props.data" :options="props.options" />
+</v-theme-provider>
+</template>
+  
+<script setup>
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
+const props = defineProps(['data', 'options'])
+</script>
