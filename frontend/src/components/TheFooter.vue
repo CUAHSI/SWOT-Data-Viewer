f4e8b62f2e583b04fde1fe8aa26130b6a1ee0710
Devin Cowan <dcowan@cuahsi.org>
2024-04-23 15:14:51 -0400

    remove data faking toggle

--- frontend/src/components/TheFooter.vue
+++ frontend/src/components/TheFooter.vue
@@ -6,24 +6,12 @@
         {{ new Date().getFullYear() }} — <strong>Swot-Data-Viewer</strong>
       </div>
       <v-spacer></v-spacer>
-      <v-switch v-model="switchValue" @click.stop="toggleSwitch" :label="`${switchValue ? 'Faking Data' : 'Using Real Data'}`"></v-switch>
     </div>
   </v-footer>
 </template>
 
 <script setup>
 import ThemeButton from './ThemeButton.vue';
-import { useFeaturesStore } from '@/stores/features'
-import { ref } from 'vue'
-
-const featureStore = useFeaturesStore()
-
-let switchValue = ref(featureStore.shouldFakeData)
-
-const toggleSwitch = () => {
-  featureStore.shouldFakeData = !featureStore.shouldFakeData
-  switchValue.value = !switchValue.value
-}
 
 </script>
 
