a346c875893443e841e7bebf77a379932e0ed883
Devin Cowan <dcowan@cuahsi.org>
2024-04-23 16:31:52 -0400

    hide login

--- frontend/src/components/TheAppBar.vue
+++ frontend/src/components/TheAppBar.vue
@@ -14,15 +14,15 @@
         </nav>
       </v-card>
       <v-spacer></v-spacer>
-      <UserLogin @logged-in="login" v-if="!mdAndDown" :mobile="false" />
+      <!-- <UserLogin @logged-in="login" v-if="!mdAndDown" :mobile="false" /> -->
 
-      <v-app-bar-nav-icon @click="$emit('toggleMobileNav')" v-else />
+      <v-app-bar-nav-icon @click="$emit('toggleMobileNav')" v-if="mdAndDown" />
     </div>
   </v-app-bar>
 </template>
 <script setup>
 import { useDisplay } from 'vuetify'
-import UserLogin from "@/components/UserLogin.vue";
+// import UserLogin from "@/components/UserLogin.vue";
 import { useAuthStore } from '../stores/auth';
 defineProps(['paths'])
 defineEmits(['toggleMobileNav'])
