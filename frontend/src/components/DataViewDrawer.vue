6a8cf1fb23e9d115caa11ee9d21faf855a9be7e7
Devin Cowan <dcowan@cuahsi.org>
2024-04-25 09:12:04 -0400

    Refactor DataViewDrawer.vue to use getFeatureName() function for displaying active feature name

--- frontend/src/components/DataViewDrawer.vue
+++ frontend/src/components/DataViewDrawer.vue
@@ -20,7 +20,7 @@
           <v-sheet class="mx-auto" elevation="8">
             <v-card v-if="featureStore.activeFeature" height="100%">
               <v-card-item class="text-center">
-                <v-card-title>{{ featureStore.activeFeature.sword.river_name }}</v-card-title>
+                <v-card-title>{{ getFeatureName() }}</v-card-title>
                 <!-- <v-card-subtitle>
                   {{ featureStore.activeFeature.sword.reach_id }}
                 </v-card-subtitle> -->
@@ -124,6 +124,15 @@ const defaultSwordMetadata = () => {
   if (!featureStore.activeFeature) return {}
   // TODO assumes reach, won't work for nodes
   return hydrologicStore.getSwordDescriptions(featureStore.activeFeature.sword, true, 'reach')
+}
+
+const getFeatureName = () => {
+  if (!featureStore.activeFeature) return ''
+  const river_name = featureStore.activeFeature.sword.river_name
+  if (river_name === 'NODATA') {
+    return 'UNNAMED REACH'
+  }
+  return river_name
 }
 
 featureStore.$subscribe((mutation, state) => {
