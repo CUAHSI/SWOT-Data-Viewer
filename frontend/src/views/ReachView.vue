a10c0cea82cd6577fa966441fa4a9f91c752f07e
Devin Cowan <dcowan@cuahsi.org>
2024-10-24 11:51:29 -0400

    add reach/:id route

---
+++ frontend/src/views/ReachView.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container fluid fill-height border="md" class="pa-6 mx-auto ma-4" max-width="1200" rounded>
+    <StaticMetadata :reachId="$route.params.reachId"/>
+  </v-container>
+</template>
+
+<script setup>
+import StaticMetadata from '@/components/StaticMetadata.vue'
+
+</script>
