af208557f95918e2d2657fd852ee2b08525c5145
Anthony Castronova <castronova.anthony@gmail.com>
2024-11-01 12:24:40 -0400

    Adding public directory to vite build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     },
+    publicDir: './public',
     assetsInclude: ['**/*.md']
   }
 })
