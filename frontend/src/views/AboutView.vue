bd967cc21d6161460b3ecc000782dc079239040c
Devin Cowan <dcowan@cuahsi.org>
2024-11-04 13:07:52 -0500

    update AboutView to fetch markdown content from assets instead of public folder

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -4,15 +4,14 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import { marked } from 'marked'; 
-//import aboutContent from '../../../documentation/about_page.md';
+import { marked } from 'marked';
+import about_page_url from '@/assets/documentation/about_page.md'
 
-//const aboutHtml = marked(aboutContent);
+// use the about_page_url to fetch the markdown content
 const aboutHtml = ref('');
 
 onMounted(async () => {
-  // Fetch the Markdown content from the public folder
-  const response = await fetch('/documentation/about_page.md');
+  const response = await fetch(about_page_url);
   const markdownText = await response.text();
   aboutHtml.value = marked(markdownText);
 });
