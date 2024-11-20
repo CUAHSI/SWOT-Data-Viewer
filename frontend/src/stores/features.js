3bc6b99a5a23859a8f3236a28e589ae1dff5471c
Devin Cowan <dcowan@cuahsi.org>
2024-11-20 14:15:13 -0500

    Revert "formatting"
    This reverts commit ede93b0ce595af342ea434f20659a500a1beb4b5.

--- frontend/src/stores/features.js
+++ frontend/src/stores/features.js
@@ -5,95 +5,94 @@ import { useChartsStore } from '@/stores/charts'
 import { useAlertStore } from './alerts'
 import { useRouter } from 'vue-router'
 
-export const useFeaturesStore = defineStore(
-  'features',
-  () => {
-    const selectedFeatures = ref([])
-    const activeFeature = ref(null)
-    const nodes = ref([])
-    // set the mintime to date of first data, 2023-03-29, relative to ECMAScript epoch in decimal seconds
-    const minTime = new Date('2023-03-29').getTime() / 1000
-    // set the maxtime to today in decimal seconds
-    const maxTime = Date.now() / 1000
+export const useFeaturesStore = defineStore('features', () => {
+  const selectedFeatures = ref([])
+  const activeFeature = ref(null)
+  const nodes = ref([])
+  // set the mintime to date of first data, 2023-03-29, relative to ECMAScript epoch in decimal seconds
+  const minTime = new Date('2023-03-29').getTime() / 1000
+  // set the maxtime to today in decimal seconds
+  const maxTime = Date.now() / 1000
 
-    const oneMonthAgoInSeconds = new Date().getTime() / 1000 - 30 * 24 * 60 * 60
-    const initialTimeRange = [oneMonthAgoInSeconds, maxTime]
-    const timeRange = ref(initialTimeRange)
-    const querying = ref({ hydrocron: false, nodes: false })
+  const oneMonthAgoInSeconds = new Date().getTime() / 1000 - 30 * 24 * 60 * 60
+  const initialTimeRange = [oneMonthAgoInSeconds, maxTime]
+  const timeRange = ref(initialTimeRange)
+  const querying = ref({ hydrocron: false, nodes: false })
 
-    const mapStore = useMapStore()
-    const chartStore = useChartsStore()
-    const alertStore = useAlertStore()
-    const router = useRouter()
+  const mapStore = useMapStore()
+  const chartStore = useChartsStore()
+  const alertStore = useAlertStore()
+  const router = useRouter()
 
-    function resetTimeRange() {
-      timeRange.value = initialTimeRange
-      chartStore.filterDatasetsToTimeRange()
+  function resetTimeRange() {
+    timeRange.value = initialTimeRange
+    chartStore.filterDatasetsToTimeRange()
+  }
+
+  function selectFeature(feature) {
+    mapStore.selectFeature(feature)
+    this.selectedFeatures.push(feature)
+    this.activeFeature = feature
+    console.log('Feature selected: ', feature)
+  }
+
+  function deselectFeature(feature) {
+    mapStore.deselectFeature(feature)
+    this.selectedFeatures = this.selectedFeatures.filter((f) => f.id !== feature.id)
+    if (this.activeFeature.id === feature.id) {
+      this.activeFeature = null
     }
+    console.log('Feature deselected: ', feature)
+  }
 
-    function selectFeature(feature) {
-      mapStore.selectFeature(feature)
-      this.selectedFeatures.push(feature)
-      this.activeFeature = feature
-      console.log('Feature selected: ', feature)
+  function mergeFeature(feature) {
+    console.log('Merging feature', feature)
+    const index = selectedFeatures.value.findIndex((f) => f.id === feature.id)
+    if (index !== -1) {
+      console.log('Feature already exists in selected features')
+      selectedFeatures.value[index] = feature
+    } else {
+      console.log('Feature does not exist in selected features')
+      selectedFeatures.value.push(feature)
     }
+    this.activeFeature = feature
+  }
 
-    function deselectFeature(feature) {
+  const clearSelectedFeatures = () => {
+    for (const feature of selectedFeatures.value) {
+      console.log('Deselecting feature', feature)
       mapStore.deselectFeature(feature)
-      this.selectedFeatures = this.selectedFeatures.filter((f) => f.id !== feature.id)
-      if (this.activeFeature.id === feature.id) {
-        this.activeFeature = null
-      }
-      console.log('Feature deselected: ', feature)
     }
+    selectedFeatures.value = []
+    activeFeature.value = null
+    mapStore.clearAllFeatures()
+    chartStore.clearChartData()
+  }
 
-    function mergeFeature(feature) {
-      console.log('Merging feature', feature)
-      const index = selectedFeatures.value.findIndex((f) => f.id === feature.id)
-      if (index !== -1) {
-        console.log('Feature already exists in selected features')
-        selectedFeatures.value[index] = feature
-      } else {
-        console.log('Feature does not exist in selected features')
-        selectedFeatures.value.push(feature)
-      }
-      this.activeFeature = feature
+  const checkFeatureSelected = (feature) => {
+    if (!selectedFeatures.value || !feature) return false
+    return selectedFeatures.value.some((f) => f?.id === feature.id)
+  }
+
+  const getFeatureName = (feature = null) => {
+    if (feature == null) {
+      feature = activeFeature.value
     }
-
-    const clearSelectedFeatures = () => {
-      for (const feature of selectedFeatures.value) {
-        console.log('Deselecting feature', feature)
-        mapStore.deselectFeature(feature)
-      }
-      selectedFeatures.value = []
-      activeFeature.value = null
-      mapStore.clearAllFeatures()
-      chartStore.clearChartData()
+    if (!feature) return ''
+    const river_name = feature.properties.river_name
+    if (river_name === 'NODATA') {
+      return 'UNNAMED RIVER'
     }
+    return river_name
+  }
 
-    const checkFeatureSelected = (feature) => {
-      if (!selectedFeatures.value || !feature) return false
-      return selectedFeatures.value.some((f) => f?.id === feature.id)
-    }
-
-    const getFeatureName = (feature = null) => {
-      if (feature == null) {
-        feature = activeFeature.value
-      }
-      if (!feature) return ''
-      const river_name = feature.properties.river_name
-      if (river_name === 'NODATA') {
-        return 'UNNAMED RIVER'
-      }
-      return river_name
-    }
-
-    const setActiveFeatureByReachId = (reachId) => {
-      // https://developers.arcgis.com/esri-leaflet/samples/querying-feature-layers-1/
-      let features = []
-      let query = mapStore.mapObject.reachesFeatures.query().where('reach_id = ' + reachId)
-      // it doesn't seem that this query.run is awaitable
-      query.run(function (error, featureCollection) {
+  const setActiveFeatureByReachId = (reachId) => {
+    // https://developers.arcgis.com/esri-leaflet/samples/querying-feature-layers-1/
+    let features = []
+    let query = mapStore.mapObject.reachesFeatures.query().where('reach_id = ' + reachId)
+    // it doesn't seem that this query.run is awaitable
+    query.run(
+      function(error, featureCollection){
         let message = ''
         if (error) {
           message = `Error querying features: ${error}`
@@ -116,28 +115,27 @@ export const useFeaturesStore = defineStore(
         let feature = features[0]
         selectedFeatures.value.push(feature)
         activeFeature.value = feature
-      })
-    }
-
-    return {
-      selectedFeatures,
-      selectFeature,
-      activeFeature,
-      clearSelectedFeatures,
-      deselectFeature,
-      checkFeatureSelected,
-      mergeFeature,
-      nodes,
-      getFeatureName,
-      setActiveFeatureByReachId,
-      timeRange,
-      resetTimeRange,
-      minTime,
-      maxTime,
-      querying
-    }
-  },
-  {
-    persist: true
+      }
+    )
   }
+
+  return {
+    selectedFeatures,
+    selectFeature,
+    activeFeature,
+    clearSelectedFeatures,
+    deselectFeature,
+    checkFeatureSelected,
+    mergeFeature,
+    nodes,
+    getFeatureName,
+    setActiveFeatureByReachId,
+    timeRange,
+    resetTimeRange,
+    minTime,
+    maxTime,
+    querying
+  }
+},
+{ persist: true }
 )
