2683a493e49be3c5e74918b627175503a2dd016a
Devin Cowan <dcowan@cuahsi.org>
2025-03-12 13:38:57 -0400

    Add notebooks module and update NotebooksView to fix vite build

--- frontend/src/views/NotebooksView.vue
+++ frontend/src/views/NotebooksView.vue
@@ -10,23 +10,23 @@
     <v-alert v-if="isDev" type="warning">
       Notebooks must be manually rendered after updates. Run ./nbconvert/nvconvert.sh to build the notebooks.
     </v-alert>
-    <v-card v-for="notebook in renderedNotebookList" :key="notebook.name" width="300">
+    <v-card v-for="notebook in notebooks" :key="notebook.name" width="300">
       <template v-slot:title>
-        <span class="font-weight-black">{{ removeExtension(notebook.name) }}</span>
+        <span class="font-weight-black">{{ removeExtension(notebook) }}</span>
       </template>
       <v-card-text class="d-flex justify-space-between">
-        <v-btn :href="`/notebooks/${notebook.name}`" text target="_blank">
+        <v-btn :href="`/notebooks/${notebook_html_from_ipynb(notebook)}`" text target="_blank">
           <v-icon left>{{ mdiNotebook }}</v-icon>
           View
           <v-tooltip activator="parent" location="bottom">
-            View rendered copy of {{ notebook.name }}
+            View rendered copy of {{ notebook_html_from_ipynb(notebook) }}
           </v-tooltip>
         </v-btn>
-        <v-btn :href="`/notebooks/${notebook_name_from_html(notebook.name)}`" download>
+        <v-btn :href="`/notebooks/${notebook}`" download>
           <v-icon left>{{ mdiDownloadBox }}</v-icon>
           Download
           <v-tooltip activator="parent" location="bottom">
-            Download {{ notebook_name_from_html(notebook.name) }}
+            Download {{ notebook }}
           </v-tooltip>
         </v-btn>
       </v-card-text>
@@ -46,37 +46,10 @@ const isDev = computed(() => process.env.NODE_ENV === 'development')
 // the public dir contains the rendered notebooks, for example:
 // http://localhost:5173/notebooks/SWOT_GISshapefiles.html
 // https://localhost/notebooks/SWOT_GISshapefiles.html
-// iterate over the files in the public/notebooks directory
-// and create a list of the files
-// this list will be used to render the links to the notebooks in the template
 
-// https://vite.dev/guide/features.html#glob-import
-const rendered_notebooks = import.meta.glob('/public/notebooks/*.html')
-let renderedNotebookList = Object.keys(rendered_notebooks).map((path) => {
-  const name = path.split('/').pop()
-  return {
-    name
-  }
-})
+import { notebooks } from '@/notebooks'
 
-const removeExtension = (name) => name.replace('.html', '')
-
-// create a list of ipynb files
-const notebook_list = import.meta.glob('/public/notebooks/*.ipynb')
-const notebookList = Object.keys(notebook_list).map((path) => {
-  const name = path.split('/').pop()
-  return {
-    name
-  }
-})
-
-const notebook_name_from_html = (html_name) => html_name.replace('.html', '.ipynb')
-
-// check that every ipynb file has a corresponding html file
-// if not, remove it
-renderedNotebookList = renderedNotebookList.filter((notebook) => {
-  const name = notebook_name_from_html(notebook.name)
-  return notebookList.some((notebook) => notebook.name === notebook_name_from_html(name))
-})
+const removeExtension = (name) => name.replace('.html', '').replace('.ipynb', '')
+const notebook_html_from_ipynb = (html_name) => html_name.replace('.ipynb', '.html')
 
 </script>
--- nbconvert/nbconvert.sh
+++ nbconvert/nbconvert.sh
@@ -3,6 +3,7 @@
 
 # export variable for the notebook directory
 export NOTEBOOK_DIR=../frontend/public/notebooks
+export SOURCE_NOTEBOOK_JS_MODULE=../frontend/src/notebooks.js
 
 # copy the notebooks from the frontend/public/notebooks directory to the notebooks directory
 mkdir -p notebooks
@@ -12,6 +13,14 @@ cp $NOTEBOOK_DIR/*.ipynb notebooks/
 docker-compose up nbconvert -d
 docker cp nbconvert:/home/jovyan/dist/. $NOTEBOOK_DIR
 docker-compose down
+
+# create a js module that has an object with the notebook names and paths
+echo "export const notebooks = [" > $SOURCE_NOTEBOOK_JS_MODULE
+for file in $NOTEBOOK_DIR/*.ipynb; do
+    filename=$(basename $file)
+    echo "  '$filename'" >> $SOURCE_NOTEBOOK_JS_MODULE
+done
+echo "];" >> $SOURCE_NOTEBOOK_JS_MODULE
 
 # remove the notebooks from the notebooks directory
 rm notebooks/*.ipynb
