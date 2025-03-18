a2b82b3d6d13bceb98fa34b95efc5978a6c30434
Devin Cowan <dcowan@cuahsi.org>
2025-03-18 14:12:28 -0400

    Add the ability to parse metadata from the notebooks

--- frontend/src/notebooks.js
+++
@@ -1,1 +0,0 @@
-export const notebooks = ['SWOT_GISshapefiles.ipynb']
--- frontend/src/views/NotebooksView.vue
+++ frontend/src/views/NotebooksView.vue
@@ -12,21 +12,22 @@
       notebooks.
     </v-alert>
     <v-row align="center" justify="center">
-      <v-col v-for="notebook in notebooks" :key="notebook.name" cols="auto">
+      <v-col v-for="notebook in notebooks" :key="notebook.filename" cols="auto">
         <v-card class="mx-auto" variant="elevated" outlined>
           <v-card-item>
             <div>
               <div class="text-overline mb-1">
                 {{ variant }}
               </div>
-              <v-card-title> {{ removeExtension(notebook) }}</v-card-title>
-              <!-- <v-card-subtitle></v-card-subtitle> -->
+              <v-card-title>{{ getTitle(notebook) }}</v-card-title>
+              <v-card-subtitle>{{ notebook.META_SUBTITLE }}</v-card-subtitle>
             </div>
           </v-card-item>
           <v-card-text>
+            {{ notebook.META_DESCRIPTION }}
             <!-- render a small view of the html -->
             <iframe
-              :src="`/notebooks/${notebook_html_from_ipynb(notebook)}`"
+              :src="`/notebooks/${notebook_html_from_ipynb(notebook.filename)}`"
               width="100%"
               height="200"
               frameborder="0"
@@ -34,23 +35,29 @@
             ></iframe>
           </v-card-text>
           <v-card-actions>
-            <v-btn :href="`/notebooks/${notebook_html_from_ipynb(notebook)}`" text target="_blank">
+            <v-btn
+              :href="`/notebooks/${notebook_html_from_ipynb(notebook.filename)}`"
+              text
+              target="_blank"
+            >
               <v-icon left>{{ mdiNotebook }}</v-icon>
               View
               <v-tooltip activator="parent" location="bottom">
-                View rendered copy of {{ notebook_html_from_ipynb(notebook) }}
+                View rendered copy of {{ notebook_html_from_ipynb(notebook.filename) }}
               </v-tooltip>
             </v-btn>
-            <v-btn :href="`/notebooks/${notebook}`" download>
+            <v-btn :href="`/notebooks/${notebook.filename}`" download>
               <v-icon left>{{ mdiDownloadBox }}</v-icon>
               Download
-              <v-tooltip activator="parent" location="bottom"> Download {{ notebook }} </v-tooltip>
+              <v-tooltip activator="parent" location="bottom">
+                Download {{ notebook.filename }}
+              </v-tooltip>
             </v-btn>
-            <v-btn :href="binder_url(notebook)" target="_blank">
+            <v-btn :href="binder_url(notebook.filename)" target="_blank">
               <v-icon left>{{ mdiRocketLaunch }}</v-icon>
               Launch
               <v-tooltip activator="parent" location="bottom">
-                Launch {{ notebook }} in Binder (JupyterHub)
+                Launch {{ notebook.filename }} in Binder (JupyterHub)
               </v-tooltip>
             </v-btn>
           </v-card-actions>
@@ -72,10 +79,14 @@ const REPO_BRANCH = 'develop'
 // http://localhost:5173/notebooks/SWOT_GISshapefiles.html
 // https://localhost/notebooks/SWOT_GISshapefiles.html
 
+// notebooks is an array of notebook objects, each with metadata
 import { notebooks } from '@/notebooks'
 
 const removeExtension = (name) => name.replace('.html', '').replace('.ipynb', '')
 const notebook_html_from_ipynb = (html_name) => html_name.replace('.ipynb', '.html')
 const binder_url = (name) =>
   `https://mybinder.org/v2/gh/CUAHSI/SWOT-Data-Viewer/${REPO_BRANCH}?urlpath=%2Fdoc%2Ftree%2Ffrontend%2Fpublic%2Fnotebooks%2F${name}`
+const getTitle = computed(
+  () => (notebook) => notebook.META_TITLE || removeExtension(notebook.filename)
+)
 </script>
