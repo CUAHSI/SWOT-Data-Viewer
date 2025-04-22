d747c53367ee30d49fd090e69d295315d1059963
Devin Cowan <dcowan@cuahsi.org>
2025-04-22 12:55:26 -0400

    Refactor NotebooksView component for improved layout and resource handling

--- frontend/src/views/NotebooksView.vue
+++ frontend/src/views/NotebooksView.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid>
+  <v-container>
     <h2>Notebooks</h2>
     <hr />
     <p class="content">
@@ -11,129 +11,126 @@
         >publicly available collection</a
       >.
     </p>
-    <v-row v-if="resourcesMetadata.length === 0" class="mb-4">
-      <v-col v-for="i in 3" :key="i" cols="12">
-        <v-card class="mx-auto" variant="elevated" outlined>
-          <v-card-item>
-            <div>
-              <v-skeleton-loader
-                class="mx-auto"
-                type="card"
-                :loading="true"
-                :height="200"
-                :width="100"
-              ></v-skeleton-loader>
-            </div>
-          </v-card-item>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row v-else class="mb-4" dense>
-      <v-col v-for="resource in resourcesMetadata" :key="resource.id">
-        <v-card class="d-flex flex-column" variant="elevated" outlined height="100%">
-          <v-card-item>
-            <v-card-title>{{ resource.title }}</v-card-title>
-            <v-card-subtitle>
-              <v-chip v-for="creator in resource.creators" :key="creator" class="mr-2" label
-                >{{ creator.name }}
-              </v-chip>
-            </v-card-subtitle>
-          </v-card-item>
-          <v-card-text>
-            <v-chip
-              v-for="subject in resource.subjects"
-              :key="subject"
-              class="mr-1"
-              small
-              color="grey lighten-1"
-              text-color="grey darken"
-              style="font-size: 0.8em"
-            >
-              {{ subject }}
-            </v-chip>
-            <div class="my-2">
-              {{ resource.abstract }}
-            </div>
-            <div class="my-2" style="color: grey">
-              {{ resource.citation }}
-            </div>
-            <div class="my-2">
-              Rendered from HydroShare Resource:
-              <a :href="`https://www.hydroshare.org/resource/${resource.id}/`" target="_blank">{{
-                resource.id
-              }}</a>
-            </div>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn v-if="resource.notebooks.length > 1">
-              <v-tooltip activator="parent" location="bottom">
-                View rendered copies at nbviewer.org
-              </v-tooltip>
-              <v-icon left>{{ mdiNotebook }}</v-icon>
-              View
-              <v-menu activator="parent">
-                <v-list
-                  dense
-                  class="pa-0"
-                  style="width: 300px"
-                  max-height="400px"
-                  overflow-y="auto"
-                >
-                  <v-list-item
-                    v-for="notebookUrl in resource.notebooks"
-                    :key="notebookUrl"
-                    :value="notebookUrl"
-                    :href="nbviewer_url(notebookUrl)"
-                    target="_blank"
-                  >
-                    <v-list-item-title>
-                      {{ notebookUrl.split('/').pop() }}
-                    </v-list-item-title>
-                  </v-list-item>
-                </v-list>
-              </v-menu>
-            </v-btn>
-            <v-btn v-else :href="nbviewer_url(resource.notebooks[0])" target="_blank">
-              <v-tooltip activator="parent" location="bottom">
-                View a rendered copy at nbviewer.org
-              </v-tooltip>
-              <v-icon left>{{ mdiNotebook }}</v-icon>
-              Preview
-            </v-btn>
-            <v-btn :href="hydroShareBagUrl(resource)" download>
-              <v-icon left>{{ mdiDownloadBox }}</v-icon>
-              Download
-              <v-tooltip activator="parent" location="bottom">
-                Download the full HydroShare resource including metadata
-              </v-tooltip>
-            </v-btn>
-            <v-btn :href="cuahsi_jh_url(resource)" target="_blank">
-              <v-icon left>{{ mdiRocketLaunch }}</v-icon>
-              Launch in CUAHSI
-              <v-tooltip activator="parent" location="bottom">
-                Launch using CUAHSI JupyterHub
-              </v-tooltip>
-            </v-btn>
-            <v-btn :href="binder_url(resource)" target="_blank">
-              <v-icon left>{{ mdiLaunch }}</v-icon>
-              Launch in MyBinder
-              <v-tooltip activator="parent" location="bottom" max-width="300">
-                MyBinder is a free service that allows you to run Jupyter notebooks in the cloud.
-                Please note the environment provided might not have all the necessary packages
-                installed. If you encounter any issues, please use the CUAHSI JupyterHub service
-                instead.
-              </v-tooltip>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
+    <template v-if="resourcesMetadata.length === 0">
+      <v-card v-for="i in 4" :key="i" class="mx-auto my-4" variant="elevated" outlined>
+        <v-card-item>
+          <div>
+            <v-skeleton-loader
+              class="mx-auto"
+              type="card"
+              :loading="true"
+              :height="200"
+              :width="100"
+            ></v-skeleton-loader>
+          </div>
+        </v-card-item>
+      </v-card>
+    </template>
+
+    <v-card
+      v-for="resource in resourcesMetadata"
+      :key="resource.id"
+      class="d-flex flex-column my-4"
+      variant="elevated"
+      outlined
+      height="100%"
+    >
+      <v-card-item>
+        <v-card-title>{{ resource.title }}</v-card-title>
+        <v-card-subtitle>
+          <v-chip v-for="creator in resource.creators" :key="creator" class="mr-2" label
+            >{{ creator.name }}
+          </v-chip>
+        </v-card-subtitle>
+      </v-card-item>
+      <v-card-text>
+        <v-chip
+          v-for="subject in resource.subjects"
+          :key="subject"
+          class="mr-1"
+          small
+          color="grey lighten-1"
+          text-color="grey darken"
+          style="font-size: 0.8em"
+        >
+          {{ subject }}
+        </v-chip>
+        <div class="my-2">
+          {{ resource.abstract }}
+        </div>
+        <div class="my-2" style="color: grey">
+          {{ resource.citation }}
+        </div>
+        <div class="my-2">
+          Rendered from HydroShare Resource:
+          <a :href="`https://www.hydroshare.org/resource/${resource.id}/`" target="_blank">{{
+            resource.id
+          }}</a>
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn v-if="resource.notebooks.length > 1">
+          <v-tooltip activator="parent" location="bottom">
+            View rendered copies at nbviewer.org
+          </v-tooltip>
+          <v-icon left>{{ mdiNotebook }}</v-icon>
+          Preview
+          <v-menu activator="parent">
+            <v-list dense class="pa-0" style="width: 300px" max-height="400px" overflow-y="auto">
+              <v-list-item
+                v-for="notebookUrl in resource.notebooks"
+                :key="notebookUrl"
+                :value="notebookUrl"
+                :href="nbviewer_url(notebookUrl)"
+                target="_blank"
+              >
+                <v-list-item-title>
+                  {{ notebookUrl.split('/').pop() }}
+                </v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-btn>
+        <v-btn v-else :href="nbviewer_url(resource.notebooks[0])" target="_blank">
+          <v-tooltip activator="parent" location="bottom">
+            View a rendered copy at nbviewer.org
+          </v-tooltip>
+          <v-icon left>{{ mdiNotebook }}</v-icon>
+          Preview
+        </v-btn>
+        <v-btn :href="hydroShareBagUrl(resource)" download>
+          <v-icon left>{{ mdiDownloadBox }}</v-icon>
+          Download
+          <v-tooltip activator="parent" location="bottom">
+            Download the full HydroShare resource including metadata
+          </v-tooltip>
+        </v-btn>
+        <v-btn :href="cuahsi_jh_url(resource)" target="_blank">
+          <v-icon left>{{ mdiRocketLaunch }}</v-icon>
+          Launch in CUAHSI
+          <v-tooltip activator="parent" location="bottom">
+            Launch using CUAHSI JupyterHub
+          </v-tooltip>
+        </v-btn>
+        <!-- TODO: once launch from HydroShare into binder is fixed, enable this -->
+        <!-- <v-btn :href="binder_url(resource)" target="_blank">
+            <v-icon left>{{ mdiLaunch }}</v-icon>
+            Launch in MyBinder
+            <v-tooltip activator="parent" location="bottom" max-width="300">
+              MyBinder is a free service that allows you to run Jupyter notebooks in the cloud.
+              Please note the environment provided might not have all the necessary packages
+              installed. If you encounter any issues, please use the CUAHSI JupyterHub service
+              instead.
+            </v-tooltip>
+          </v-btn> -->
+      </v-card-actions>
+    </v-card>
   </v-container>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import { mdiDownloadBox, mdiNotebook, mdiRocketLaunch, mdiLaunch } from '@mdi/js'
+import { mdiDownloadBox, mdiNotebook, mdiRocketLaunch } from '@mdi/js'
 // TODO use the collection
 // import { VITE_HYDROSHARE_NOTEBOOKS_COLLECTION } from '@/constants'
 const VITE_HYDROSHARE_NOTEBOOKS_COLLECTION = 'ac6cc75dcb0146cf9cc17a974f4bb08b'
@@ -160,6 +157,12 @@ onMounted(async () => {
           }
         }
       })
+      // filter out undefined values
+      resourceIds = resourceIds.filter((id) => id !== undefined)
+      // remove duplicates
+      resourceIds = [...new Set(resourceIds)]
+      // sort the resource ids
+      resourceIds.sort()
       console.log('Found the following resources in the collection', resourceIds)
       return resourceIds
     })
@@ -185,12 +188,12 @@ const nbviewer_url = (notebookUrl) => {
   return `https://nbviewer.org/urls/${notebookUrl}`
 }
 
-const binder_url = (resource) => {
-  // the hydroshare binder functionality is currently broken
-  // return `https://mybinder.org/v2/hydroshare/${resource.id}`
-  // instead, rely on https://github.com/hydroshare/hydroshare_github_sync
-  return `https://mybinder.org/v2/gh/hydroshare/hydroshare_github_sync/main?urlpath=/doc/tree/${resource.id}/data/contents`
-}
+// TODO: the hydroshare binder functionality is currently broken
+// const binder_url = (resource) => {
+//   // return `https://mybinder.org/v2/hydroshare/${resource.id}`
+//   // instead, rely on https://github.com/hydroshare/hydroshare_github_sync
+//   return `https://mybinder.org/v2/gh/hydroshare/hydroshare_github_sync/main?urlpath=/doc/tree/${resource.id}/data/contents`
+// }
 
 const notebooks_in_resource = async (resourceId) => {
   // for the resource, get a list of all the notebook files
